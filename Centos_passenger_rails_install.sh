9d131153633f65c6b7b9ae632c5c42a08a850082
justinz <justinz@yeah.net>
2019-03-04 12:57:43 +0800

    Update Centos_passenger_rails_install.sh

--- Centos_passenger_rails_install.sh
+++ Centos_passenger_rails_install.sh
@@ -10,7 +10,7 @@ service ntpd start
 yum install -y pygpgme curl
 curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
 yum install -y nginx passenger || sudo yum-config-manager --enable cr && sudo yum install -y nginx passenger
-yum install -y nodejs vim git
+yum install -y nodejs vim git mysql-devel
 # 配置 /etc/nginx/conf.d/passenger.conf 文件
 # passenger-config --root
 service nginx restart && passenger-memory-stats
