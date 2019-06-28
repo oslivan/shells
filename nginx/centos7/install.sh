542db77abd88a44b6d4586c0cd1cc373b819861c
justinz <justinz@yeah.net>
2019-06-28 13:38:31 +0800

    Update install.sh

--- nginx/centos7/install.sh
+++ nginx/centos7/install.sh
@@ -1,4 +1,4 @@
-curl -o /etc/yum.repos.d/nginx.repo -sL https://raw.githubusercontent.com/oslivan/shells/master/nginx/centos7/nginx.repo
+sudo curl -o /etc/yum.repos.d/nginx.repo -sL https://raw.githubusercontent.com/oslivan/shells/master/nginx/centos7/nginx.repo
 sudo yum update
 sudo yum install -y nginx
 sudo nginx
