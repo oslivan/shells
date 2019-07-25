cef75b8ecb914abf74e9ca52845a33aacefb5ed9
justinz <justinz@yeah.net>
2019-07-25 11:54:49 +0800

    Update install.sh

--- nginx/centos7/install.sh
+++ nginx/centos7/install.sh
@@ -1,4 +1,4 @@
 sudo curl -o /etc/yum.repos.d/nginx.repo -sL https://git.io/fjolh
-sudo yum update
+sudo yum update -y
 sudo yum install -y nginx
 sudo systemctl enable nginx && sudo systemctl start nginx
