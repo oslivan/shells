78b583b5dd0b2bcd34cb43c293c6aa41fa1e90da
justinz <msgsub@163.com>
2020-03-13 23:09:23 +0800

    Update install.sh

--- ruby/mac/install.sh
+++ ruby/mac/install.sh
@@ -6,6 +6,7 @@ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(rbenv init -)"' >> ~/.zshrc
 source ~/.zshrc
 mkdir -p "$(rbenv root)"/plugins
-git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/andorchen/rbenv-china-mirror.git ~/.rbenv/plugins/rbenv-china-mirror # change to china mirror
 rbenv install 2.6.4 && rbenv global 2.6.4
 gem install bundler --no-doc
