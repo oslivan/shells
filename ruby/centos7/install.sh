44a674fe0e124d7d57c4489fc865d4cdc180395a
justinz <msgsub@163.com>
2020-09-29 09:54:07 +0800

    Update install.sh

--- ruby/centos7/install.sh
+++ ruby/centos7/install.sh
@@ -4,8 +4,8 @@ git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 source ~/.bash_profile
-mkdir -p ～/.rbenv/plugins
-git clone https://github.com/rbenv/ruby-build.git ～/.rbenv/plugins/ruby-build
+mkdir -p ~/.rbenv/plugins
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 git clone https://github.com/andorchen/rbenv-china-mirror.git ~/.rbenv/plugins/rbenv-china-mirror # change to china mirror
 rbenv install 2.5.1 && rbenv global 2.5.1
 gem install bundler --no-doc
