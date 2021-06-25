c453ae22a43d5bea0fbcd4ce148b9b1d085a6244
Justin <justinz@yeah.net>
2021-06-25 10:24:00 +0800

    更新mac安装ruby

--- ruby/macos/install.sh
+++ ruby/macos/install.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 # xcode-select --install
-# brew install openssl libyaml libffi
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(rbenv init -)"' >> ~/.zshrc
 source ~/.zshrc
 mkdir -p "$(rbenv root)"/plugins
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-git clone https://github.com/andorchen/rbenv-china-mirror.git ~/.rbenv/plugins/rbenv-china-mirror # change to china mirror
-rbenv install 2.6.4 && rbenv global 2.6.4
+# 如果没有翻墙可以开启下面这行
+# git clone https://github.com/andorchen/rbenv-china-mirror.git ~/.rbenv/plugins/rbenv-china-mirror # change to china mirror
+brew install openssl readline libyaml libffi
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+rbenv install 2.6.6 && rbenv global 2.6.6
 gem install bundler --no-doc
