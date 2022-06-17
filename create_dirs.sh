5949761488b44abe13a81c6acc451a914e023868
Justin <msgsub@163.com>
2022-06-17 09:58:09 +0800

    Create create_dirs.sh

---
+++ create_dirs.sh
@@ -0,0 +1,11 @@
+create_dir() {
+  mkdir -p documents projects softwares videos pictures audios other
+  mkdir -p documents/maikeji documents/personal
+  mkdir -p projects/maikeji projects/personal
+  mkdir -p softwares/centos softwares/ubuntu softwares/windows softwares/macOS softwares/android
+  mkdir -p videos/network videos/personal
+  mkdir -p pictures/network pictures/personal
+  mkdir -p audios/network audios/personal
+}
+
+create_dir
